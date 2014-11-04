df68ae4a96a4a46e987265a4da54e26a785631d2
Salmon <grass.house@hotmail.com>
2014-11-04 18:12:11 +0800

    Fixed the error fifth and add the submit button.

--- salmon_edited/style/salmon/fixedButton.js
+++ salmon_edited/style/salmon/fixedButton.js
@@ -5,7 +5,7 @@ $(document).on('mousemove', function(e) {
     var h = $(this).height();
     var angle = Math.atan2(y-(h), x-(w)) * (180/Math.PI);
     var rotate = angle + (180-45);
-    $('.button .compass')
+    $('.button.submitButton .compass')
         .css('-webkit-transform', 'rotate('+rotate+'deg)')
         .css('-moz-transform', 'rotate('+rotate+'deg)')
         .css('-ms-transform', 'rotate('+rotate+'deg)')
